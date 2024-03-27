0e82075fa3800457b7ec27bd29fb312b6749ab41
aryazaki052 <yogaarya020@gmail.com>
2024-03-27 11:28:56 +0800

    tugas no 1

--- pert4/condition.java
+++ pert4/condition.java
@@ -1,23 +1,54 @@
+// import java.util.Scanner;
+
 import java.util.Scanner;
 
 public class condition {
-  public static void main(String[] args) {
-    // declare variable
-    int angka; 
-    Scanner ipt = new Scanner(System.in);
+  // public static void main(String[] args) {
+  //   // declare variable
+  //   int angka; 
+  //   Scanner ipt = new Scanner(System.in);
 
-    // catch value from user
-    System.out.println("Inputkan Bilangan:");
-    angka = ipt.nextInt();
+  //   // catch value from user
+  //   System.out.println("Inputkan Bilangan:");
+  //   angka = ipt.nextInt();
 
-    // proces compare number
-    if(angka % 2 == 0){
-      System.out.println(angka + " Adalah Bilangan Genap");
-    }else{
-      System.out.println(angka + " Adalah Bilangan Ganjil");
-    }
+  //   // proces compare number
+  //   if(angka % 2 == 0){
+  //     System.out.println(angka + " Adalah Bilangan Genap");
+  //   }else{
+  //     System.out.println(angka + " Adalah Bilangan Ganjil");
+  //   }
 
     
-    ipt.close();
+  //   ipt.close();
+  // }
+
+    public static void main(String[] args) {
+      // declare variable
+      int hargaBarang;
+      double hargaDiskon = 0.0;
+      int diskonMaksimal = 40000;
+      Scanner ipt = new Scanner(System.in);
+      final double PERSENDISC = 0.2;
+
+      // catch value from the user
+      System.out.println("Inputkan Harga Barang");
+      hargaBarang = ipt.nextInt();
+
+      // logika
+      if (hargaBarang > 40000) {
+          hargaDiskon = hargaBarang * PERSENDISC;
+          if (hargaDiskon > diskonMaksimal) {
+              hargaDiskon = diskonMaksimal;
+          }
+      } else {
+          // Jika harga barang kurang dari atau sama dengan 40000, maka tidak ada diskon
+          hargaDiskon = 0.0;
+      }
+
+      double hargaAkhir = hargaBarang - hargaDiskon;
+      System.out.println("Harga Akhir nya adalah " + hargaAkhir);
+
+      ipt.close();
   }
 }
